f3190106906c06826a8eb8614fe249863632cb06
Layal Haidar <layal.haidar1@hotmail.com>
2021-06-04 12:34:57 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -32,3 +32,27 @@ UPDATE students
 SET Points = 190
 WHERE name = "Alex";
 
+
+CREATE TABLE graduates(
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+Name varchar(255), 
+Age INT,  
+Gender varchar(255), 
+Points INT, 
+Graduation varchar(255)
+);
+
+
+insert into graduates (Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points
+       FROM students 
+	where students.name = "Layal"
+
+
+UPDATE graduates 
+SET Graduation = '08/09/2018'
+WHERE name = "Layal"
+
+
+DELETE FROM students
+WHERE name="Layal"
